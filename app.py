f2ef9e14e52b187af869a11ce29541a0c2816518
Muhamad Wildan <muhamadwildan226@gmail.com>
2026-09-07 09:24:10 +0700

    chore: add spaces GPU bypass for zeroGPU forced hardware

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ import sys
 import os
 import uvicorn
 
-# Tambahkan folder backend ke path agar import di main.py tidak error
 sys.path.append(os.path.join(os.path.dirname(__file__), "backend"))
+
+# Dummy function untuk mengelabui ZeroGPU Hugging Face
+try:
+    import spaces
+    import torch
+    @spaces.GPU
+    def init_gpu():
+        pass
+    init_gpu()
+except Exception as e:
+    print("Bypass GPU:", e)
 
 from backend.main import app
 
